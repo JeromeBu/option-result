6f8884e4135620cd58f4c44157856efde442eadd
Jérôme Burkard <burkardjerome@gmail.com>
2021-05-20 17:53:35 +0200

    initiate library

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { some, none, Option } from "./option";
+export { someAsync, noneAsync, OptionAsync } from "./optionAsync";
+export { ok, err, Result } from "./result";
+export { okAsync, errAsync, ResultAsync } from "./resultAsync";
